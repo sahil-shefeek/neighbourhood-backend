fb6ab90c08317a3d2bb2191941e0d0ad3c3462f2
rozalia <rozaliacv32@gmail.com>
2024-11-01 15:32:02 +0530

    routes added

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,9 +2,13 @@ import express from "express";
 import userRouter from "./users/users.js";
 import authRouter from "./auth/auth.js";
 import verifyJWT from "../../middleware/auth/verifyJWT.js";
+import productRouter from "./productRoutes/products/products.js";
+import serviceRouter from "./serviceRoutes/services/services.js";
 
 export const apiRouter = express.Router();
 
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/users", userRouter);
 apiRouter.use(verifyJWT);
+apiRouter.use("/products",productRouter);
+apiRouter.use("/services",serviceRouter);
